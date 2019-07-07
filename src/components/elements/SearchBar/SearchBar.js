0f61241a7c81b29342d6a56757a42faf974b28b9
Matt Kilcup <nevermindthelabel@gmail.com>
2019-07-06 22:21:39 -0700

    added search bar component and logic

---
+++ src/components/elements/SearchBar/SearchBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import FontAwesome from 'react-fontawesome';
+import './SearchBar.css';
+
+export default class SearchBar extends React.Component {
+  state = {
+    value: ''
+  };
+
+  timeout = null;
+
+  handleSearch = event => {
+    this.setState({
+      value: event.target.value
+    });
+    clearTimeout(this.timeout);
+
+    this.timeout = setTimeout(() => {
+      this.props.callback(this.state.value);
+    }, 500);
+  };
+
+  render() {
+    return (
+      <div className="rmdb-searchbar">
+        <div className="rmdb-serchbar-content">
+          <FontAwesome className="rmdb-fa-search" name="search" size="2x" />
+          <input
+            type="text"
+            className="rmdb-searchbar-input"
+            placeholder="Search"
+            onChange={this.handleSearch}
+            value={this.state.value}
+          />
+        </div>
+      </div>
+    );
+  }
+}
