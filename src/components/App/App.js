75428abb28341bea367c520ba8e25df544395e86
Matt Kilcup <nevermindthelabel@gmail.com>
2019-07-07 19:14:33 -0700

    added react router and routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Header from '../elements/Header/Header';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NotFound from '../elements/NotFound/NotFound'
+// import Movie from '../../pages/Movie/Movie';
 import Home from '../../pages/Home/Home';
 
 const App = () => {
   return(
-    <div>
-      <Header />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          {/* <Route exact path="/:movieId" component={Movie} /> */}
+          <Route component={NotFound} />
+        </Switch>
+      </React.Fragment>
+    </BrowserRouter>
   );
 };
 
