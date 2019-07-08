22d25f841e5381526f65332bb22d864155719152
Matt Kilcup <nevermindthelabel@gmail.com>
2019-07-07 21:28:09 -0700

    initial movie page

---
+++ src/pages/Movie/Movie.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Movie extends Component {
+  state = {
+
+  }
+  render() {
+    return (
+      <div>
+        Movie
+      </div>
+    )
+
+  }
+}
+
+export default Movie;
