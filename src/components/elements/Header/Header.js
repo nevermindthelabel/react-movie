47e9653ca95ab9db8aace2b4e6423c26dac39cf7
Matt Kilcup <nevermindthelabel@gmail.com>
2019-07-07 21:58:37 -0700

    changed paths to dynamicly add image

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../../../images/reactMovie_logo.png';
+import TMDBLogo from '../../../images/tmdb_logo.png';
 import './Header.css';
 
 const Header = () => {
@@ -7,10 +9,10 @@ const Header = () => {
     <div className="rmdb-header">
       <div className="rmdb-header-content">
         <Link to="/">
-          <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="rmdb logo" />
+          <img className="rmdb-logo" src={Logo} alt="rmdb logo" />
         </Link>
         <a href="https://www.themoviedb.org/">
-          <img src="./images/tmdb_logo.png" alt="tmdb logo" className="rmdb-tmdb-logo" />
+          <img src={TMDBLogo} alt="tmdb logo" className="rmdb-tmdb-logo" />
         </a>
       </div>
     </div>
