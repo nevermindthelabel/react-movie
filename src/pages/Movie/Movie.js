c1a82544159289747159d7f07efed35b07699f41
Matt Kilcup <nevermindthelabel@gmail.com>
2019-07-08 21:27:37 -0700

    inital movie page setup

--- src/pages/Movie/Movie.js
+++ src/pages/Movie/Movie.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-
+import { URL, API_KEY } from '../../config';
+import Navigation from '../../components/elements/Navigation/Navigation';
+import MovieInfo from '../../components/elements/MovieInfo/MovieInfo';
+import MovieInfoBar from '../../components/elements/MovieInfo/MovieInfoBar';
+import FourColGrid from '../../components/elements/FourColGrid/FourColGrid';
+import Actor from '../../components/elements/Actor/Actor';
+import Spinner from '../../components/elements/Spinner/Spinner';
 class Movie extends Component {
   state = {
 
