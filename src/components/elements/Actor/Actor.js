84854fcfc5e415e90ef8ca8a2c8da6d05d905fc8
Matt Kilcup <nevermindthelabel@gmail.com>
2019-08-10 07:59:59 -0700

    added logic for actor component

--- src/components/elements/Actor/Actor.js
+++ src/components/elements/Actor/Actor.js
@@ -3,11 +3,21 @@ import { IMAGE_URL } from '../../../config';
 import './Actor.css';
 
 const Actor = props => {
+  const POSTER_SIZE = 'w154';
   return (
-    <div>
-      Actor
+    <div className="rmdb-actor">
+      <img
+        src={
+          props.actor.profile_path
+            ? `${IMAGE_URL}${POSTER_SIZE}/${props.actor.profile_path}`
+            : './images/no_image.jpg'
+        }
+        alt="actor"
+      />
+      <span className="rmdb-actor-name">{props.actor.name}</span>
+      <span className="rmdb-actor-character">{props.actor.character}</span>
     </div>
-  )
-}
+  );
+};
 
 export default Actor;
