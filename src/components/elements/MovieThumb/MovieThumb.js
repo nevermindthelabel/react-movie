8b59a5a88462c1f33fc39934674bf1231a3f552c
Matt Kilcup <nevermindthelabel@gmail.com>
2019-08-10 12:51:46 -0700

    added proptypes

--- src/components/elements/MovieThumb/MovieThumb.js
+++ src/components/elements/MovieThumb/MovieThumb.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './MovieThumb.css';
 
 const MovieThumb = props => {
-  return(
-    <div className='rmdb-moviethumb'>
-      <img src={props.image} alt={props.movieName}/>
+  console.log(props)
+  return (
+    <div className="rmdb-moviethumb">
+      {props.clickable ? (
+        <Link to={{ pathname: `/movie/${props.movieId}`, movieName: `${props.movieName}` }}>
+          <img src={props.image} alt={props.movieName} />
+        </Link>
+      ) : (
+        <img src={props.image} alt={props.movieName} />
+      )}
     </div>
-  )
+  );
+};
+
+MovieThumb.propTypes = {
+  image: PropTypes.string,
+  movieId: PropTypes.number,
+  movieName: PropTypes.string
+
 }
 
 export default MovieThumb;
