b9a921c85639e41fe62b72623bbd5d09534487fc
Matt Kilcup <nevermindthelabel@gmail.com>
2019-08-09 21:32:09 -0700

    added movie info content

---
+++ src/components/elements/MovieInfo/MovieInfo.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { IMAGE_URL, POSTER_SIZE, BACKDROP_SIZE } from '../../../config';
+import FontAwesome from 'react-fontawesome';
+import MovieThumb from '../../elements/MovieThumb/MovieThumb';
+
+const MovieInfo = props => {
+  return (
+    <div
+      className="rmdb-movieinfo"
+      style={{
+        background: props.movie.backdrop_path
+          ? `url('${IMAGE_URL}${BACKDROP_SIZE}${props.movie.backdrop_path}')`
+          : '#000'
+      }}
+    >
+      <div className="rmdb-movieinfo-content">
+        <div className="rmdb-movieinfo-thumb">
+          <MovieThumb
+            image={
+              props.movie.poster_path
+                ? `${IMAGE_URL}${POSTER_SIZE}${props.image.poster_path}`
+                : './images/no_image.jpg'
+            }
+            clickable={false}
+          />
+        </div>
+        <div className="rmdb-movieinfo-text">
+          <h1>{props.movie.title}</h1>
+          <h3>Plot</h3>
+          <p>{props.movie.overview}</p>
+          <h3>IMDB Rating</h3>
+          <div className="rmdb-rating">
+            <meter
+              min="0"
+              max="100"
+              optimum="100"
+              low="40"
+              high="70"
+              value={props.movie.vote_average * 10}
+            />
+            <p className="rmdb-score">{props.movie.vote_average}</p>
+          </div>
+          {props.directors.length > 1 ? <h3>Directors</h3> : <h3>Director</h3>}
+          {props.directors.map((director, index) => {
+            return <p key={index} className="rmdb-director">{director.name}</p>
+          })}
+        </div>
+        <FontAwesome className="fa-film" name="film" size="5x" />
+      </div>
+    </div>
+  );
+};
+
+export default MovieInfo;
