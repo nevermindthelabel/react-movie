553c376308324fdcba101fb476b207b9d9dd06f3
Matt Kilcup <nevermindthelabel@gmail.com>
2019-08-11 15:33:50 -0700

    destructuring props

--- src/components/elements/Navigation/Navigation.js
+++ src/components/elements/Navigation/Navigation.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
 
-const Navigation = props => {
+const Navigation = ({ movie }) => {
   return (
     <div className="rmdb-navigation">
       <div className="rmdb-navigation-content">
         <Link to="/">
           <p>Home</p>
           <p>/</p>
-          {props.movie}
+          {movie}
         </Link>
       </div>
     </div>
